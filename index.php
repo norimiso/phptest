83b7cf7abafc52515c0fa1a862101d66ab7fc519
norimiso <norimiso@gmail.com>
2017-02-01 14:37:55 +0900

    4th commit

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
-<?php
-if(isset($_GET[‘comment’])){
-$comment = $_GET[‘comment’];
-echo $comment;
-}
-?>
-<!DOCTYPE html>
-<html lang = “ja”>
-<head>
-<meta charset = “UFT-8”>
-<title>フォームからデータを受け取る</title>
-</head>
+<html>
+<head><title>input.html</title></head>
 <body>
-<h1>フォームデータの送信</h1>
-<form action = “index.php” method = “get”>
-<input type = “text” name =“comment/“><br/>
-<input type = “submit” value =“送信/“>
+入力フォームです。
+名前を入力してみましょう。
+<form action="output.php" method="post">
+  <table border="1">
+    <tr>
+      <td>名前</td>
+      <td><input type="text" name="name"></td>
+      <td colspan="2" align="center">
+        <input type="submit" value="入力">
+      </td>
+    </tr>
+  </table>
 </form>
 </body>
 </html>
